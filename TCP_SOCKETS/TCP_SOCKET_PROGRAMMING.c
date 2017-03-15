e3d6cc672eef1c39da0e284b2f28565aab058391
Ali Gholami <aligholami7596@gmail.com>
2017-03-15 12:33:22 +0330

    Socket opening was added

---
+++ TCP_SOCKETS/TCP_SOCKET_PROGRAMMING.c
@@ -0,0 +1,27 @@
+/*
+*  Socket Programming with C
+* ========================================
+* [] File Name : TCP_SOCKET_PROGRAMMING.c
+*
+* [] Creation Date : March 2017
+*
+* [] Created By : Ali Gholami (aligholami7596@gmail.com)
+* ========================================
+*/
+
+#include <stdio.h>
+#include <sys/socket.h>
+
+int main(int argc,char argv*[])
+{
+	/*
+    *  socket system call
+	*  Address Familiy - AF_INET(IP Ver 4)
+	*  Type - SOCK_STREAM(connection oriented TCP Protocol)
+	*  Protocol - 0(IP Protocol)  
+	*  return value: socket descriptor
+	*/
+	int socketDsc = socket(AF_INET,SOCK_STREAM,0);
+}
+
+
