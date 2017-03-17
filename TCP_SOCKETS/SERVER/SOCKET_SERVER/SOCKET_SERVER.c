5042b2704be6018ad9679ef35a8fbfa5056bbdd2
Ali Gholami <aligholami7596@gmail.com>
2017-03-18 01:17:15 +0330

    Configure the socket server

--- TCP_SOCKETS/SERVER/SOCKET_SERVER/SOCKET_SERVER.c
+++ TCP_SOCKETS/SERVER/SOCKET_SERVER/SOCKET_SERVER.c
@@ -16,12 +16,19 @@ int main()
 	int socketDsc;
 	struct sockaddr_in server;
 	
+	/* Create the socket server */
 	socketDsc = socket(AF_INET, SOCK_STREAM, 0);
 	if(socketDsc == -1)
 	{
 		printf("Cannot create the socket");
 		return 1;
 	}
+	
+	/* Configure the socket */
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(8888);
+	
 	
 	
 }
