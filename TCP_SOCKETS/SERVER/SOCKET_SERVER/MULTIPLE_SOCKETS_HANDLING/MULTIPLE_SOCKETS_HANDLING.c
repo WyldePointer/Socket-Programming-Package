a8d42fb4ac399f91ad58b57b3e873247513fec9b
Ali Gholami <aligholami7596@gmail.com>
2017-03-19 11:16:22 +0330

    Start handling multiple sockets with threads

---
+++ TCP_SOCKETS/SERVER/SOCKET_SERVER/MULTIPLE_SOCKETS_HANDLING/MULTIPLE_SOCKETS_HANDLING.c
@@ -0,0 +1,9 @@
+/* Socket Programming with C
+* ========================================
+* [] File Name : MULTIPLE_SOCKETS_HANDLING.c
+*
+* [] Creation Date : March 2017
+*
+* [] Created By : Ali Gholami (aligholami7596@gmail.com)
+* ========================================
+*/
