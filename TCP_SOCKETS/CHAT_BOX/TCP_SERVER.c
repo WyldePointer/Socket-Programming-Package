6f39a489da2862e0dfc7a399a5ac031f7be2de47
methoz <aligholami7596@gmail.com>
2017-03-24 12:21:52 +0430

    Define variables

--- TCP_SOCKETS/CHAT_BOX/TCP_SERVER.c
+++ TCP_SOCKETS/CHAT_BOX/TCP_SERVER.c
@@ -7,3 +7,24 @@
 * [] Created By : Ali Gholami (aligholami7596@gmail.com)
 * ========================================
 */
+
+#include <stdio.h>
+#include <stdib.h>
+
+
+
+
+int main()
+{
+	/* Define variables */
+	int sockDsc, newsockDsc;
+	struct sockaddr_in addr, client_addr;
+	char buffer[BUF_SIZE];
+	pid_t childpid;
+	char clientAddr[CL_ADDR_LEN];
+	pthread_t thread;
+
+
+}
+
+	
