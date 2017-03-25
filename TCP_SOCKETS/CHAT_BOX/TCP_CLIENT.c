d071e507fb8c332b507b42acb71865c3c61b8804
methoz <aligholami7596@gmail.com>
2017-03-25 18:59:02 +0430

    add header files

--- TCP_SOCKETS/CHAT_BOX/TCP_CLIENT.c
+++ TCP_SOCKETS/CHAT_BOX/TCP_CLIENT.c
@@ -8,3 +8,18 @@
 * ========================================
 */
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+
+int main()
+{
+		
+}
